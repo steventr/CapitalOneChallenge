3452b5b8a8817e5d6229e8b35b540ae797c9c711
Steven Truong <steventr@usc.edu>
2015-10-30 23:54:58 -0700

    First Commit

--- server/startup.js
+++ server/startup.js
@@ -3,7 +3,7 @@
  */
 if ( Meteor.isServer ) {
     Meteor.startup(function () {
-        Instagram.setClientID('89639b8f13a04ea097f36d9bff1dc7aa');
-        AlchemyAPI.setClientID('2f0fa8fa3c294238b9d24aab0d34f7135bcf0a13');
+        Instagram.setClientID('');
+        AlchemyAPI.setClientID('');
     });
 }
